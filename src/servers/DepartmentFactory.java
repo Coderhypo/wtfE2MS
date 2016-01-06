45b9005a817d81eae79f2806287d3aa9eb490ee6
Coderhypo <i@ihypo.net>
2016-01-06 15:51:45 +0800

    add factory

--- src/servers/DepartmentFactory.java
+++ src/servers/DepartmentFactory.java
@@ -1,7 +1,32 @@
 package servers;
 
+import models.Department;
+
 /**
+ * 部门工厂类 提供创建合法部门的方法
  * Created by hypo on 16-1-6.
  */
 public class DepartmentFactory {
+
+    private String code;
+    private String name;
+
+    public DepartmentFactory(String code, String name) {
+        setName(name);
+        setCode(code);
+    }
+
+    public Department getDepartment(){
+        Department department = new Department(code, name);
+
+        return department;
+    }
+
+    private void setCode(String code) {
+        this.code = code.substring(0,10);
+    }
+
+    private void setName(String name) {
+        this.name = name.substring(0,20);
+    }
 }
--- src/servers/EmployeeFactory.java
+++ src/servers/EmployeeFactory.java
@@ -1,7 +1,45 @@
 package servers;
 
+import models.Employee;
+
 /**
+ * 员工工厂类 提供创建员工的方法
  * Created by hypo on 16-1-6.
  */
 public class EmployeeFactory {
+
+    private String code;
+    private String name;
+    private String sex;
+    private Integer dpId;
+
+    public EmployeeFactory(String code, String name, String sex, Integer dpId) {
+        setCode(code);
+        setName(name);
+        setSex(sex);
+        setDpId(dpId);
+    }
+
+    public Employee getEmployee(){
+
+        Employee employee = new Employee(code,name,sex, dpId);
+
+        return employee;
+    }
+
+    private void setCode(String code) {
+        this.code = code.substring(0,10);
+    }
+
+    private void setName(String name) {
+        this.name = name.substring(0,20);
+    }
+
+    private void setSex(String sex) {
+        this.sex = sex.substring(0,10);
+    }
+
+    private void setDpId(Integer dpId) {
+        this.dpId = dpId;
+    }
 }

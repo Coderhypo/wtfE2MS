997b3371cdb62ab3fffcff21ccda4fafe731073f
Coderhypo <i@ihypo.net>
2016-01-07 22:07:50 +0800

    finish test

--- src/test/testDepartment.java
+++ src/test/testDepartment.java
@@ -1,7 +1,37 @@
 package test;
 
+import dao.DepartmentDAO;
+import dao.IDAO;
+import models.Department;
+import servers.DepartmentFactory;
+
+import java.util.List;
+
 /**
  * Created by hypo on 16-1-7.
  */
 public class testDepartment {
+
+    public static void main(String[] args){
+        get();
+
+    }
+
+    public static void add(){
+        IDAO dao = new DepartmentDAO();
+
+        Department department = new DepartmentFactory("abc", "Test").getDepartment();
+
+        System.out.println(department.getId());
+    }
+
+    public static void get(){
+        IDAO dao = new DepartmentDAO();
+        String HQL = "FROM Department d";
+        List<Department> departments = dao.query(HQL);
+
+        for(Department department : departments){
+            System.out.println(department.getId());
+        }
+    }
 }
--- src/test/testMamager.java
+++ src/test/testMamager.java
@@ -1,7 +1,37 @@
 package test;
 
+import dao.IDAO;
+import dao.ManagerDAO;
+import models.Manager;
+import tools.PasswordTool;
+
 /**
  * Created by hypo on 16-1-7.
  */
 public class testMamager {
+
+    public static void main(String[] agrs){
+
+        get();
+
+        return;
+    }
+
+    public static void add(){
+        IDAO dao = new ManagerDAO();
+        Manager manager = new Manager("admin", "ADMIN", PasswordTool.getPass("admin"));
+
+        dao.add(manager);
+
+    }
+
+    public static void get(){
+
+        IDAO dao = new ManagerDAO();
+        String HQL = "FROM Manager m WHERE m.login='" + "admin" + "'";
+//        String HQL = "FROM Manager m WHERE m.id=" + 1;
+        Manager manager = (Manager) dao.get(HQL);
+        System.out.println(manager.getId());
+
+    }
 }
